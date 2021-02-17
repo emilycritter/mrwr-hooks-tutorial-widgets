931921f357c7f3a4e597c08130feeb9645f2d0bb
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-17 10:02:57 -0600

    Adds hardcoded array to App.js and passees content as a prop to Accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javasript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among developers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You user React by creating components'
+  }
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items}/>
+    </div>
+  );
 }
