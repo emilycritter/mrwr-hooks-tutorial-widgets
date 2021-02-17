33e60c33a9a728ffb5ffb0ace2b7246d3d8b6208
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-17 10:47:43 -0600

    Uses activeIndex state variable to finish accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null); // syntax for array destructuring, where null is the default value for activeIndex
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const onTitleClick = (index) => {
     setActiveIndex(index);
   }
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={items.title}>
         <div
-          className="title active"
+          className={`title ${active}`}
           onClick={() => onTitleClick(index)}
         >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
@@ -27,7 +29,6 @@ const Accordion = ({ items }) => {
   return (
     <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
   );
 }
