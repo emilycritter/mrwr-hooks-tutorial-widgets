3cdeb5b8f68367144436707531fa2b07b2238bea
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-19 10:41:14 -0600

    Resolve console warning in Search component ('React Hook useEffect has a missing dependency')

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,27 +24,16 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         }
       });
 
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
-
-      return () => { // This function runs before the component is re-rendered
-        clearTimeout(timeoutId); // Cancel the previous timeout
-      }
+    if(debouncedTerm) {
+      search();      
     }
-  }, [term]);
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
