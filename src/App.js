512d99ea692551f3b07a05186a3d5bcab79b69d4
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-20 12:42:44 -0600

    Extracts routing logic into new Route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -34,50 +35,28 @@ const options = [
   }
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-}
-
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-}
-
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return (
-      <div>
-      {/*
-          <Dropdown
-           label="Select a Color"
-           selected={selectedColor}
-           onSelectedChange={setSelectedColor}
-           options={options}
-         />
-      */}
-      </div>
-    );
-  }
-}
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-}
-
 export default () => {
   const [selectedColor, setSelectedColor] = useState(options[0]);
 
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+         label="Select a Color"
+         selected={selectedColor}
+         onSelectedChange={setSelectedColor}
+         options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
