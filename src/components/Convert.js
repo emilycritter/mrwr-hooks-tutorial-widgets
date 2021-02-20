9b71652cc632ca0792a275ff8c59af0b9504a3f5
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-20 12:23:16 -0600

    Adds debouncedText to state to throttle API calls to Google Translate

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,11 +4,23 @@ const API_KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [text]);
+
   useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
         params: {
-          q: text,
+          q: debouncedText,
           target: language.value,
           key: API_KEY
         }
@@ -17,10 +29,12 @@ const Convert = ({ language, text }) => {
     };
 
     doTranslation();
-  }, [language, text]); // Run any time we get a new value for language or text
+  }, [language, debouncedText]); // Run any time we get a new value for language or debouncedText
 
   return (
-    <div><h1 className="ui header">{translated}</h1></div>
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
   );
 };
 
