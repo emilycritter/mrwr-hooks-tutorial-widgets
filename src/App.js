31358c2b891d80370e7dc1c25dd1cbb29fceae06
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-20 11:53:47 -0600

    Adds new Translate component that includes a Dropdown as a child component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -41,7 +42,6 @@ export default () => {
     <div>
       {/* <Accordion items={items} />
       <Search />
-       */}
        <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
        {showDropdown ?
          <Dropdown
@@ -50,6 +50,8 @@ export default () => {
           options={options}
         /> : null
        }
+       */}
+      <Translate />
     </div>
   );
 }
