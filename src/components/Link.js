c0c9e0953b90a6524c88a835d4448fa2a0a9836e
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-20 12:59:13 -0600

    Adds event from Link component that is listened for in the Route component so that we can eventually respond to changes in navigation (ie nav element clicks)

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
-    window.history.pushState({}, '', href); // Changes URL without refreshing the page
+    window.history.pushState({}, '', href); // Changes URL without refreshing the page (Note: window.history.pushState is a function available to the browser)
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log('change');
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    }
+  }, []); // Only run when component is first rendered to the screen
+
   return window.location.pathname === path
     ? children
     : null;
