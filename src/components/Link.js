dedf38ce40380f68e3072b1c71877ddf734b701c
Emily Ritter <emily.c.ritter@gmail.com>
2021-02-20 13:04:02 -0600

    Handles command clicks (to open tabs in new window)

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) { // Boolean values to detect if user wants to open link in new tab
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState({}, '', href); // Changes URL without refreshing the page (Note: window.history.pushState is a function available to the browser)
 
